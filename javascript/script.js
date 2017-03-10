cb95c4ea337c517f3c6dc4211c423c77af2463f4
Sergio Redondo Montes <sergio.redondo@live.u-tad.com>
2017-03-10 10:26:07 +0100

    Arreglo json

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-        var url = "sistemas.json";
+        var url = "./json/sistemas.json";
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 var myArr = JSON.parse(this.responseText);
