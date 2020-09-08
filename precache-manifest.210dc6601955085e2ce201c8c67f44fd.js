daaa020cb60cc5955f64415c2560d6c560fcdb8f
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-09-08 12:42:27 +0200

    Update Portfolio

---
+++ precache-manifest.210dc6601955085e2ce201c8c67f44fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff74df5a9d7c5d6b1d2191ae5a559f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0b872cbe1029642edd5",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b872cbe1029642edd5",
+    "url": "/static/js/main.1e7a0773.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
